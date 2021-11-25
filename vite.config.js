fbd5dc95c5722f97125b33dc5b9f37b27693ec51
Jayden Sim <39719057+immutableshrub@users.noreply.github.com>
2021-11-25 14:28:07 +1100

    Fixed https certifucate madness

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import { readFileSync } from 'fs'
 
-export default defineConfig({
-    server: {
-        host: true,
-        port: 1673,
-        https: {
-            key: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert-key.pem"),
-            cert: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert.pem")
-        },
+
+export default defineConfig(({ command, mode }) => {
+    if (command === 'serve') {
+        return {
+            server: {
+                host: true,
+                port: 1673,
+                https: {
+                    key: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert-key.pem"),
+                    cert: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert.pem")
+                },
+            }
+        }
+    } else {
+        return {}
     }
-});
+})
